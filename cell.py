a19d7fee7ee5e54aa48fc79efc25254242d17a4e
chenxiaota <czj01@vip.qq.com>
2019-09-04 16:25:33 +0800

    修改了cell，将总执行变成了函数。添加了getlola部分函数需要接受参数cityid,dayid,citylevel

--- cell.py
+++ cell.py
@@ -3,48 +3,81 @@ import datetime
 import getlola
 import threading
 
-if __name__ == '__main__':
+
+def exec_cell(cityId, cityLevel, dayId):
     threadl = []
-    sqlTruncateMid = "truncate table B_CELL_MID"
-    getlola.Oracle_Exec(sqlTruncateMid)
-
-    #获取基站的名称和经纬度
-    cell_lo_la = getlola.get_cell_lo_la('1000513')
-    #获取指定区域网格的id、name、point
-    orgid_orgname_point = getlola.get_point_2('1000513','3')
+    # 获取基站的名称和经纬度
+    cell_lo_la = getlola.get_cell_lo_la(cityId, dayId)
+    # 获取指定区域网格的id、name、point
+    orgid_orgname_point = getlola.get_point_2(cityId, cityLevel)
     orgid_orgname_point = getlola.expand_orgidnamepoint(orgid_orgname_point)
-    #print(orgid_orgname_point[0])
-    # 对station进行分组
 
-    flag = int(len(cell_lo_la)/8)
-    cellList1 = [ x for x in cell_lo_la[:flag]]
-    cellList2 = [ x for x in cell_lo_la[flag:flag*2]]
-    cellList3 = [ x for x in cell_lo_la[flag*2:flag*3]]
-    cellList4 = [ x for x in cell_lo_la[flag*3:flag*4]]
+    flag = int(len(cell_lo_la) / 8)
+    cellList1 = [x for x in cell_lo_la[:flag]]
+    cellList2 = [x for x in cell_lo_la[flag:flag * 2]]
+    cellList3 = [x for x in cell_lo_la[flag * 2:flag * 3]]
+    cellList4 = [x for x in cell_lo_la[flag * 3:flag * 4]]
     cellList5 = [x for x in cell_lo_la[flag * 4:flag * 5]]
     cellList6 = [x for x in cell_lo_la[flag * 5:flag * 6]]
     cellList7 = [x for x in cell_lo_la[flag * 6:flag * 7]]
     cellList8 = [x for x in cell_lo_la[flag * 7:]]
 
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList1, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList2, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList3, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList4, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList5, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList6, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList7, orgid_orgname_point,'3',)))
-    threadl.append(threading.Thread(target=getlola.in_cell_mid_table, args=(cellList8, orgid_orgname_point,'3',)))
+    if flag > 0:
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList1, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList2, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList3, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList4, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList5, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList6, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList7, orgid_orgname_point, cityLevel, dayId, cityId,)))
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList8, orgid_orgname_point, cityLevel, dayId, cityId,)))
+    else:
+        threadl.append(threading.Thread(target=getlola.in_cell_mid_table,
+                                        args=(cellList8, orgid_orgname_point, cityLevel, dayId, cityId,)))
 
     for i in threadl:
         i.start()
     for i in threadl:
         i.join()
 
-    #将基站信息找到的网格录入中间表
-    #getlola.in_station_mid_table(station_lo_la,orgid_orgname_point)
-    #对比中间表修改正式表中基站的grid_id
-    #test.updata_station_all()
-    #getlola.conn.close()
+
+if __name__ == '__main__':
+
+    cityLevel = '3'
+    dayId = '20190828'
+    threadl = []
+    # sqlTruncateMid = "truncate table B_CELL_MID"
+    # getlola.Oracle_Exec(sqlTruncateMid)
+
+    # print(orgid_orgname_point[0])
+    # 对station进行分组
+
+    # 将基站信息找到的网格录入中间表
+    # getlola.in_station_mid_table(station_lo_la,orgid_orgname_point)
+    # 对比中间表修改正式表中基站的grid_id
+    # test.updata_station_all()
+    # getlola.conn.close()
+
+    listForCityId = ['1000250', '1000510', '1000511', '1000512', '1000513', '1000514', '1000515', '1000516', '1000517',
+                     '1000518', '1000519', '1000523', '1000527']
+
+    for cityId in listForCityId:
+        try:
+            print(cityId,"执行开始：",cityLevel,datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+            exec_cell(cityId,cityLevel,dayId)
+            print(cityId, "==========执行结束：", datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+        except:
+            print(cityId,"执行失败 fail")
+            print(cityId,"==========执行结束：",datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+            continue
 
     endTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
     print("开始时间为：" + getlola.startTime)
--- getlola.py
+++ getlola.py
@@ -3,18 +3,19 @@ import cx_Oracle
 import datetime
 import os
 
+# 设置字符集与oracle一致，不然insert中文乱码
 os.environ['NLS_LANG'] = 'AMERICAN_AMERICA.ZHS16GBK'
-print('beging...')
+print('====beging...')
 # 获取需要判断的数据信息
 startTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+print('===========================',startTime,'============================')
 
 DB_UserName = "js_gis"
 DB_UserPwd = "I6ftcQbeCFWyHK7"
 DB_ConnectStr = "10.32.241.203/xemkt1"
 
 
-
-def Oracle_Query(SqlStr, debug = 0):
+def Oracle_Query(SqlStr, debug=0):
     "Execute oracle query, and return data_list"
     conn = cx_Oracle.connect(DB_UserName, DB_UserPwd, DB_ConnectStr)
     data_list = []
@@ -37,8 +38,9 @@ def Oracle_Query(SqlStr, debug = 0):
         print("Exec sql failed: %s" % SqlStr)
     finally:
         cursor.close()
-        #conn.close()
+        # conn.close()
         return data_list
+
 
 def Oracle_Exec(SqlStr):
     "Execute oracle command"
@@ -54,9 +56,10 @@ def Oracle_Exec(SqlStr):
         return False
     finally:
         cursor.close()
-    #conn.close()
+    # conn.close()
 
-#判断坐标是否再坐标圈内
+
+# 判断坐标是否再坐标圈内
 def is_pt_in_poly(aLon, aLat, pointList):
     '''''
     :param aLon: double 经度
@@ -67,7 +70,7 @@ def is_pt_in_poly(aLon, aLat, pointList):
     iCount = len(pointList)
     if (iCount < 3):
         return False
-    #print("iCount = " + str(iCount))
+    # print("iCount = " + str(iCount))
     for i in range(iCount):
         pLon1 = pointList[i][0]
         pLat1 = pointList[i][1]
@@ -75,7 +78,7 @@ def is_pt_in_poly(aLon, aLat, pointList):
             pLon2 = pointList[0][0]
             pLat2 = pointList[0][1]
         else:
-            #print(i+1)
+            # print(i+1)
             try:
                 pLon2 = pointList[i + 1][0]
                 pLat2 = pointList[i + 1][1]
@@ -104,7 +107,8 @@ def is_pt_in_poly(aLon, aLat, pointList):
     else:
         return False
 
-#格式化从excel中得到的内容
+
+# 格式化从excel中得到的内容
 # def get_file_row(row,file_name):
 #     wb_1 = openpyxl.load_workbook('%s' % file_name)
 #     ws_1 = wb_1.active
@@ -115,15 +119,15 @@ def is_pt_in_poly(aLon, aLat, pointList):
 #         list_2.append(i.value)
 #     return list_2
 
-#查询获得基站的名称、经度、纬度
+# 查询获得基站的名称、经度、纬度
 def get_station_lo_la():
-
     sql_sec_name_lo_la = "SELECT A.B_STATION_NAME,A.LONGITUDE,A.LATITUDE FROM B_BASE_STATION_INFO A"
     list_station_lo_la = Oracle_Query(sql_sec_name_lo_la)
     return list_station_lo_la
 
-#查询获得网格的id、名称、网格坐标圈 指定市
-def get_point_2(org_id,orgLevel):
+
+# 查询获得网格的id、名称、网格坐标圈 指定市
+def get_point_2(org_id, orgLevel):
     sql_sec_orgid_orgname_point = "select r.org_code,r.org_name,r.point " \
                                   "from (" \
                                   "select a.org_code,a.org_name,a.p_org_id,a.org_level,b.point " \
@@ -135,11 +139,12 @@ def get_point_2(org_id,orgLevel):
                                   "select org_id from s_orgnization start with org_id='%s' " \
                                   "connect by prior org_id=p_org_id)) r,s_orgnization o " \
                                   "where r.p_org_id=o.org_id " \
-                                  "and r.org_level='%s'" % (str(org_id),str(orgLevel))
+                                  "and r.org_level='%s'" % (str(org_id), str(orgLevel))
     list_orgid_orgname_point = Oracle_Query(sql_sec_orgid_orgname_point)
     return list_orgid_orgname_point
 
-#查询获得网格的id、名称、网格坐标圈
+
+# 查询获得网格的id、名称、网格坐标圈
 def get_point():
     sql_sec_orgid_orgname_point = "select a.org_id,a.org_name,b.point from s_orgnization a " \
                                   "left join p_map_draw b on b.org_id = a.org_id" \
@@ -147,7 +152,8 @@ def get_point():
     list_orgid_orgname_point = Oracle_Query(sql_sec_orgid_orgname_point)
     return list_orgid_orgname_point
 
-#格式化cblob字段
+
+# 格式化cblob字段
 def expand_list(tList):
     mList = tList.read().split(";")
     flag = 0
@@ -156,16 +162,17 @@ def expand_list(tList):
         flag += 1
     return mList
 
-#修改基站grid_id   bak表
-def update_station_grid_id(gridId,bStationName):
 
+# 修改基站grid_id   bak表
+def update_station_grid_id(gridId, bStationName):
     sqlUpdateGridId = "update b_base_station_info_bak a " \
                       "set a.grid_id='%s' " \
-                      "where a.b_station_name='%s'" % (str(gridId),bStationName)
+                      "where a.b_station_name='%s'" % (str(gridId), bStationName)
     updateResult = Oracle_Exec(sqlUpdateGridId)
     return updateResult
 
-#对比两个b_station_name的grid_id是否相同
+
+# 对比两个b_station_name的grid_id是否相同
 def judge_station_name(stationName):
     sqlGridIdFromInfo = " select grid_id from b_base_station_info_bak where b_station_name = '%s'" % stationName
     sqlGridIdFromMid = "select grid_id from B_STATION_GRID_MID where b_station_name = '%s'" % stationName
@@ -176,7 +183,8 @@ def judge_station_name(stationName):
     else:
         return gridIdFromMid
 
-#格式化orgIdNamePoint
+
+# 格式化orgIdNamePoint
 def expand_orgidnamepoint(orgIdNamePoint):
     flag = 0
     for i in orgIdNamePoint:
@@ -186,21 +194,22 @@ def expand_orgidnamepoint(orgIdNamePoint):
         else:
             continue
         flag += 1
-    return  orgIdNamePoint
+    return orgIdNamePoint
 
-#获取数据入中间表
-def in_station_mid_table(stationLoLa,orgIdNamePoint):
 
-    for station_name, station_lo, station_la in stationLoLa: #获取基站的经纬度
-        for ord_id, org_name, org_point in orgIdNamePoint:   #获取网格的相关内容 (id、name、point list)
+# 获取数据入中间表
+def in_station_mid_table(stationLoLa, orgIdNamePoint):
+    for station_name, station_lo, station_la in stationLoLa:  # 获取基站的经纬度
+        for ord_id, org_name, org_point in orgIdNamePoint:  # 获取网格的相关内容 (id、name、point list)
             judge_result = is_pt_in_poly(station_lo, station_la, org_point)
             if judge_result:
                 sql_insert_b_station_grid_mid = "insert into b_station_grid_mid (org_name,grid_id,b_station_name) " \
-                                                "values ('%s','%s','%s')" % (org_name,ord_id,station_name)
+                                                "values ('%s','%s','%s')" % (org_name, ord_id, station_name)
                 Oracle_Exec(sql_insert_b_station_grid_mid)
                 break
 
-#对照中间表修改正式表中的所有数据
+
+# 对照中间表修改正式表中的所有数据
 def updata_station_all():
     sqlSecStationNameFromMid = "select b_station_name from B_STATION_GRID_MID"
     stationNameList = Oracle_Query(sqlSecStationNameFromMid)
@@ -209,12 +218,13 @@ def updata_station_all():
         if gridId:
             print(stationNameTup[0])
             print(gridId)
-            update_station_grid_id(gridId[0][0],stationNameTup[0])
+            update_station_grid_id(gridId[0][0], stationNameTup[0])
         else:
             continue
 
-#获取小区的名称以及经纬度
-def get_cell_lo_la(cityId,dayID):
+
+# 获取小区的名称以及经纬度
+def get_cell_lo_la(cityId, dayID):
     """
     :param cityId: 小区所在的城市ID
     :return:
@@ -224,29 +234,31 @@ def get_cell_lo_la(cityId,dayID):
                        "WHERE CITY_ID='%s' " \
                        "AND A.LONGITUDE IS NOT NULL " \
                        "AND A.LATITUDE IS NOT NULL " \
-                       "AND A.DAY_ID='%s'" % (str(cityId),str(dayID))
-    print(sqlGetCellIdLoLa)
+                       "AND A.DAY_ID='%s'" % (str(cityId), str(dayID))
     listCellLoLa = Oracle_Query(sqlGetCellIdLoLa)
-    return  listCellLoLa
+    return listCellLoLa
 
-#判断小区的结果并录入中间表
-def in_cell_mid_table(cellLoLa,orgIdNamePoint,orgLevel):
+
+# 判断小区的结果并录入中间表
+def in_cell_mid_table(cellLoLa, orgIdNamePoint, orgLevel, dayId, cityId):
     """
     :param cellLoLa: 小区的坐标信息
     :param orgIdNamePoint: 网格的坐标范围信息
     :param orgLevel: 网格等级
     :return: 无返回值
     """
-    for cellId, cellLo, cellLa in cellLoLa: #获取基站的经纬度
-        for ord_id, org_name, org_point in orgIdNamePoint:   #获取网格的相关内容 (id、name、point list)
+    for cellId, cellLo, cellLa in cellLoLa:  # 获取基站的经纬度
+        for ord_id, org_name, org_point in orgIdNamePoint:  # 获取网格的相关内容 (id、name、point list)
             judge_result = is_pt_in_poly(cellLo, cellLa, org_point)
             if judge_result:
-                sql_insert_b_cell_mid = "insert into b_cell_mid (org_name,grid_id,org_level,cell_id) " \
-                                                "values ('%s','%s','%s','%s')" % (org_name,ord_id,str(orgLevel),cellId)
+                sql_insert_b_cell_mid = "insert into b_cell_mid (day_id,org_name,grid_id,city_id,org_level,cell_id) " \
+                                        "values ('%s','%s','%s','%s','%s','%s')" % (
+                                        str(dayId), org_name, ord_id, str(cityId), str(orgLevel), cellId)
                 Oracle_Exec(sql_insert_b_cell_mid)
                 break
 
-#对比两个cell_id的region_id是否相同
+
+# 对比两个cell_id的region_id是否相同
 def judge_cell_id_region(cellId):
     sqlGridIdFromInfo = " select region from B_SUBDISTRICT_INFO where cell_id = '%s'" % cellId
     sqlGridIdFromMid = "select grid_id from B_CELL_MID where cell_id = '%s'" % cellId
@@ -257,16 +269,17 @@ def judge_cell_id_region(cellId):
     else:
         return gridIdFromMid
 
-#修改基站grid_id   bak表
-def update_cell_region(gridId,cellId):
 
+# 修改基站grid_id   bak表
+def update_cell_region(gridId, cellId):
     sqlUpdateGridId = "update B_SUBDISTRICT_INFO a " \
                       "set a.region='%s' " \
-                      "where a.cell_id='%s'" % (str(gridId),cellId)
+                      "where a.cell_id='%s'" % (str(gridId), cellId)
     updateResult = Oracle_Exec(sqlUpdateGridId)
     return updateResult
 
-#对照中间表修改正式表中的所有数据 update小区region_id
+
+# 对照中间表修改正式表中的所有数据 update小区region_id
 def updata_cell_region_all():
     sqlSecCellIdFromMid = "select cell_id from B_CELL_MID"
     cellIdList = Oracle_Query(sqlSecCellIdFromMid)
@@ -275,13 +288,6 @@ def updata_cell_region_all():
         if gridId:
             print(cellIdTup[0])
             print(gridId)
-            update_cell_region(gridId[0][0],cellIdTup[0])
+            update_cell_region(gridId[0][0], cellIdTup[0])
         else:
             continue
-
-
-
-
-
-
-
